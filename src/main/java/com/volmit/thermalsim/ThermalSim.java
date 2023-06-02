f18032303c905385c81d2bdb6d54c842bea1cae1
DanMB <danielmillst@gmail.com>
2023-06-02 11:20:11 -0500

    f

---
+++ src/main/java/com/volmit/thermalsim/ThermalSim.java
@@ -0,0 +1,15 @@
+package com.volmit.thermalsim;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ThermalSim extends JavaPlugin {
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
